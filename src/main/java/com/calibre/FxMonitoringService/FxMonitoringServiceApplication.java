5f8731ab2def839d080c7fdfd285b5b4caa99d2e
kms97 <kms97.ks@gmail.com>
2023-09-10 16:40:27 +1000

    [KS]: Init spring project

---
+++ src/main/java/com/calibre/FxMonitoringService/FxMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.calibre.FxMonitoringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calibre/FxMonitoringService/FxMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calibre.FxMonitoringService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
